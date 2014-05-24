6804adaec3ff9765f6ebd63df2b7d81dd9862904
Rcmaniac25 <rcmaniac25@hotmail.com>
2014-05-24 00:20:42 -0400

    Initial commit

---
+++ DotBlackBerry/BPS/BPS.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Runtime.InteropServices;
+
+namespace blackberry.BPS
+{
+    /// <summary>
+    /// BlackBerry Platform Services
+    /// </summary>
+    public class BPS
+    {
+        #region PInvoke
+
+        [DllImport("bps")]
+        static extern int bps_get_version();
+
+        #endregion
+
+        /// <summary>
+        /// BPS Version
+        /// </summary>
+        public static Version Version
+        {
+            get
+            {
+                // The version number is computed as follows:
+                // (Major * 1000000) + (Minor * 1000) + Patch
+                var encodedVersion = bps_get_version();
+                var major = encodedVersion / 1000000;
+                var majorEncoded = major * 1000000;
+                var minor = (encodedVersion - majorEncoded) / 1000;
+                var patch = encodedVersion - (majorEncoded + (minor * 1000));
+                return new Version(major, minor, patch);
+            }
+        }
+
+        //TODO
+    }
+}
---
+++ DotBlackBerry/Properties/AssemblyInfo.cs
@@ -0,0 +1,31 @@
+﻿using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+// General Information about an assembly is controlled through the following 
+// set of attributes. Change these attribute values to modify the information
+// associated with an assembly.
+[assembly: AssemblyTitle("BlackBerry")]
+[assembly: AssemblyProduct("DotBlackBerry")]
+[assembly: AssemblyCopyright("Copyright © Vincent Simonetti 2014")]
+
+// Setting ComVisible to false makes the types in this assembly not visible 
+// to COM components.  If you need to access a type in this assembly from 
+// COM, set the ComVisible attribute to true on that type.
+[assembly: ComVisible(false)]
+
+// The following GUID is for the ID of the typelib if this project is exposed to COM
+[assembly: Guid("69bd37bc-979b-4c9c-a2aa-afb8971e2531")]
+
+// Version information for an assembly consists of the following four values:
+//
+//      Major Version
+//      Minor Version 
+//      Build Number
+//      Revision
+//
+// You can specify all the values or you can default the Build and Revision Numbers 
+// by using the '*' as shown below:
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("0.1.0.0")]
+[assembly: AssemblyFileVersion("0.1.0.0")]
