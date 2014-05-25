998f711c40f6f2ecfee3bcfc5c332778ae18912c
Rcmaniac25 <rcmaniac25@hotmail.com>
2014-05-24 22:26:30 -0400

    Added BPS functions: InitializeLogging, SetLoggingVerbosity, RegisterDomain, and RegisterShutdownHandler.

--- DotBlackBerry/BPS/BPS.cs
+++ DotBlackBerry/BPS/BPS.cs
@@ -4,6 +4,34 @@ using System.Runtime.InteropServices;
 namespace BlackBerry.BPS
 {
     /// <summary>
+    /// Verbosity levels for BPS logging.
+    /// </summary>
+    [AvailableSince(10, 0)]
+    public enum BPSVerbosity : uint
+    {
+        /// <summary>
+        /// Shutdown and Critical messages are logged.
+        /// </summary>
+        [AvailableSince(10, 0)]
+        Critical = 0,
+        /// <summary>
+        /// Shutdown, Critical, Error and Warning messages are logged.
+        /// </summary>
+        [AvailableSince(10, 0)]
+        ErrorWarning = 1,
+        /// <summary>
+        /// Shutdown, Critical, Error, Warning, Notice and Info messages are logged.
+        /// </summary>
+        [AvailableSince(10, 0)]
+        InfoNotice = 2,
+        /// <summary>
+        /// Shutdown, Critical, Error, Warning, Notice, Info, Debug1 and Debug2 messages are logged.
+        /// </summary>
+        [AvailableSince(10, 0)]
+        Debug = 3
+    }
+
+    /// <summary>
     /// BlackBerry Platform Services
     /// </summary>
     [AvailableSince(10, 0)]
@@ -12,7 +40,7 @@ namespace BlackBerry.BPS
         #region PInvoke
 
         [DllImport("bps")]
-        internal static extern int bps_get_version();
+        private static extern int bps_get_version();
 
         [DllImport("bps")]
         internal static extern int bps_initialize();
@@ -22,6 +50,18 @@ namespace BlackBerry.BPS
 
         [DllImport("bps")]
         internal static extern void bps_free(IntPtr ptr);
+
+        [DllImport("bps")]
+        private static extern int bps_logging_init();
+
+        [DllImport("bps")]
+        private static extern void bps_set_verbosity(uint verbosity);
+
+        [DllImport("bps")]
+        private static extern int bps_register_domain();
+
+        [DllImport("bps")]
+        private static extern int bps_register_shutdown_handler(Action<IntPtr> callback, IntPtr data);
 
         #endregion
 
@@ -64,6 +104,8 @@ namespace BlackBerry.BPS
             disposed = false;
         }
 
+        #region Property
+
         /// <summary>
         /// BPS Version
         /// </summary>
@@ -98,6 +140,8 @@ namespace BlackBerry.BPS
             }
         }
 
+        #endregion
+
         /// <summary>
         /// Shutdown BPS
         /// </summary>
@@ -123,9 +167,90 @@ namespace BlackBerry.BPS
             }
         }
 
-        //TODO
+        #region Functions
+
+#if BLACKBERRY_INTERNAL_FUNCTIONS
+
+        /// <summary>
+        /// Setup a default slog2 buffer if one is not set.
+        /// </summary>
+        /// <returns>true if a default slog2 buffer was set up successfully or if one was already setup. false if setting up a default slog2 buffer failed.</returns>
+        [AvailableSince(10, 1)]
+        public bool InitializeLogging()
+        {
+            return bps_logging_init() == BPS_SUCCESS;
+        }
+
+#endif
+
+        /// <summary>
+        /// Set the verbosity of logging for platform services.
+        /// </summary>
+        /// Default verbosity is BPSVerbosity.InfoNotice.
+        /// <param name="verbosity">The desired verbosity</param>
+        [AvailableSince(10, 0)]
+        public void SetLoggingVerbosity(BPSVerbosity verbosity)
+        {
+            bps_set_verbosity((uint)verbosity);
+        }
+
+        /// <summary>
+        /// Reserve a unique domain ID for identifying a service's events.
+        /// </summary>
+        /// <returns>A domain ID for a service. A value of -1 indicates an error.</returns>
+        [AvailableSince(10, 0)]
+        public int RegisterDomain()
+        {
+            return bps_register_domain();
+        }
+
+        private static void ShutdownHandler(IntPtr data)
+        {
+            var parsedData = Util.DeserializeFromPointer(data);
+            Util.FreeSerializePointer(data);
+            if (parsedData != null)
+            {
+                var parts = parsedData as object[];
+                if (parts != null)
+                {
+                    var callback = parts[0] as Action<object>;
+                    callback(parts[1]);
+                }
+                else
+                {
+                    var callback = parsedData as Action<object>;
+                    callback(null);
+                }
+            }
+        }
+
+        /// <summary>
+        /// Register a callback that will be invoked when the last shutdown function is called.
+        /// </summary>
+        /// <param name="callback">Callback to invoke on shutdown.</param>
+        /// <param name="callbackData">Additional data to pass to callback.</param>
+        /// <returns>true is returned if the handler registered successfully with the data, false if otherwise.</returns>
+        [AvailableSince(10, 0)]
+        public bool RegisterShutdownHandler(Action<object> callback, object callbackData = null)
+        {
+            var toSerialize = callbackData == null ? (object)callback : new object[] { callback, callbackData };
+            var data = Util.SerializeToPointer(toSerialize);
+            if (data == IntPtr.Zero)
+            {
+                return false;
+            }
+            return bps_register_shutdown_handler(ShutdownHandler, data) == BPS_SUCCESS;
+        }
+
+        //TODO: BPSChannel CreateChannel(...) //bps_channel_create
+
+        //TODO: bool GetEvent(..., out BPSEvent ev) //bps_get_event
+
+        //TODO: bool PushEvent(BPSEvent ev) //bps_push_event
 
         //XXX should possibly have Equals, HashCode, and ToString. The issue is all BPS instances should be the the same, 
         //but if a BPS instance shouldn't be disposable, then it is not equal and should not be treated as such.
+
+        #endregion
     }
 }
--- DotBlackBerry/Util.cs
+++ DotBlackBerry/Util.cs
@@ -1,5 +1,9 @@
 ﻿using System;
-//using System.Runtime.InteropServices;
+using System.IO;
+using System.Runtime.Serialization;
+using System.Runtime.Serialization.Formatters.Binary;
+using System.Runtime.InteropServices;
+
 using Mono.Unix.Native;
 
 namespace BlackBerry
@@ -21,6 +25,83 @@ namespace BlackBerry
             }
             //TODO
             throw new Exception(string.Format("An exception has occured with the error code: {0:X}", errno));
+        }
+
+        /// <summary>
+        /// Serialize an object to a pointer.
+        /// </summary>
+        /// <param name="obj">The object to serialize.</param>
+        /// <returns>The data, or IntPtr.Zero if obj is null or an error occurs.</returns>
+        public static IntPtr SerializeToPointer(object obj)
+        {
+            if (obj == null)
+            {
+                return IntPtr.Zero;
+            }
+
+            // Serialize
+            var formatter = new BinaryFormatter();
+            var ms = new MemoryStream();
+            try
+            {
+                formatter.Serialize(ms, obj);
+            }
+            catch
+            {
+                return IntPtr.Zero;
+            }
+
+            // Check data length
+            if (ms.Length > Int32.MaxValue)
+            {
+                return IntPtr.Zero;
+            }
+
+            // Copy to pointer
+            var data = ms.GetBuffer();
+            var ptr = Syscall.malloc((ulong)ms.Length + sizeof(int));
+            Marshal.WriteInt32(ptr, (int)ms.Length);
+            Marshal.Copy(data, 0, ptr, (int)ms.Length);
+            return ptr;
+        }
+
+        /// <summary>
+        /// Deserialize an object from a pointer.
+        /// </summary>
+        /// <param name="ptr">The pointer to deserialize.</param>
+        /// <returns>The object or null if the pointer is IntPtr.Zero or an error has occured.</returns>
+        public static object DeserializeFromPointer(IntPtr ptr)
+        {
+            if (ptr == IntPtr.Zero)
+            {
+                return null;
+            }
+
+            // Get data
+            var dataLength = Marshal.ReadInt32(ptr);
+            var data = new byte[dataLength + sizeof(int)];
+            Marshal.Copy(ptr, data, 0, dataLength + sizeof(int));
+            var ms = new MemoryStream(data, sizeof(int), dataLength, false);
+
+            // Deserialize
+            var formatter = new BinaryFormatter();
+            try
+            {
+                return formatter.Deserialize(ms);
+            }
+            catch
+            {
+                return null;
+            }
+        }
+
+        /// <summary>
+        /// Free the serialized pointer (just a wrapper around Syscall.free)
+        /// </summary>
+        /// <param name="ptr">The pointer to free.</param>
+        public static void FreeSerializePointer(IntPtr ptr)
+        {
+            Syscall.free(ptr);
         }
     }
 }
